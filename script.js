44eef29680571bce127df7d4ae81a08f9ebc16e9
William Muhirwa <muhirwawilliam245@gmail.com>
2024-08-28 15:58:26 +0200

    change

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.getElementById('score--0');
 const score1El = document.getElementById('score--1');
 const current0El = document.getElementById('current--0');
@@ -15,10 +17,12 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let ActivePlayer = 0;
 
 // rolling dice functionality
-btnRoll.addEventListener('click', function() {
+btnRoll.addEventListener('click', function() {``
   // 1. Generating random dice roll
   const dice = Math.trunc(Math.random() * 6) + 1;
 
@@ -31,12 +35,27 @@ btnRoll.addEventListener('click', function() {
   if(dice !== 1) {
     // add dice to current score
     currentScore += dice;
-    current1El.textContent = currentScore;
+    document.getElementById(`current--${ActivePlayer}`).textContent = currentScore;
   } else {
     // switch to next player
+    document.getElementById(`current--${ActivePlayer}`).textContent = 0;
+
+    ActivePlayer = ActivePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0El.classList.toggle('player--active');
+    player0El.classList.toggle('player--active');
+
   }
-
-
-
   }
 );
+
+btnHold.addEventListener('click', function () {
+
+  // // 1. add current score to active
+  scores(activePlayer) += currentScore;
+  score[1] = score[1]
+  // 2. check if player's score is >= 100
+  // finish the Game
+  // switch to the next player
+
+})
